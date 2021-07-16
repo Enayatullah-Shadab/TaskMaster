297c736323ab3f4342e2e904adc2c10eb17089fc
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-07-16 17:23:20 -0600

    fix

--- app/Controllers/TmasterController.js
+++ app/Controllers/TmasterController.js
@@ -7,12 +7,7 @@ function _draw() {
     let taskMaster = ProxyState.TaskMaster;
     let template = ''
     taskMaster.forEach(t => template += t.Template)
-    document.getElementById("app").innerHTML = /*html*/`
-  <button className="btn btn-info" onclick="app.TaskMasterController.createTask()">CreteTask</button>  
-  <div className="card-columns TaskMaster">
-      ${template}
-  </div>
-  `
+    document.getElementById("app").innerHTML = template
 }
 
 //Public
@@ -22,9 +17,9 @@ export default class TaskMasterController {
         _draw()
     }
 
-    CreteTask(event) {
+    createTask(event) {
         event.preventDefault()
-        taskMasterService.CreteTask()
+        taskMasterService.createTask()
         console.log("button clicked")
     }
 
